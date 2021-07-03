e07819f83272ed366c8b02096dda9f6acbceba24
cocoo7458@gmail.com <cocoo7458@gmail.com>
2021-07-03 20:06:22 +0700

    Add project files.

---
+++ CuaHangDienTu2012B.sql
@@ -0,0 +1,186 @@
+﻿/*
+Created: 27/10/2020
+Modified: 01/07/2021
+Model: Microsoft SQL Server 2014
+Database: MS SQL Server 2014
+*/
+use master
+
+----------
+create database Electronic
+ON
+	(NAME='Electronic_DATA',
+	FILENAME='E:\CSDL\dienmay\Electronic.MDF')
+LOG ON
+	(NAME='LINHKIEN_LOG',
+	FILENAME='E:\CSDL\dienmay\Electronic.LDF')
+GO
+use Electronic
+GO
+
+-- Create tables section -------------------------------------------------
+
+-- Table KhachHang
+
+CREATE TABLE KhachHang
+(
+ MaKhach Int IDENTITY(1,1) NOT FOR REPLICATION NOT NULL primary key,
+ TenKhach Varchar(100) NULL,
+ DiaChi Varchar(100) NULL,
+ DiaChiEmail Varchar(100) NULL,
+ SoDienThoai Varchar(100) NULL,
+ NgaySinh Date NULL,
+ TinhTrang Char(100) NULL,
+ TaiKhoan Varchar(100) NULL,
+ MatKhau Varchar(100) NULL
+)
+go
+
+-- Add keys for table KhachHang
+
+ALTER TABLE [KhachHang] ADD CONSTRAINT [PK_KhachHang] PRIMARY KEY ([MaKhach])
+go
+
+-- Table DonHang
+
+CREATE TABLE DonHang
+(
+ MaDonHang Int IDENTITY(1,1) NOT FOR REPLICATION NOT NULL primary key ,
+ TongSoLuong Int NULL,
+ TongGiaBan Int NULL,
+ TinhTrang Nvarchar(100) NULL,
+ MaKhach Int NOT NULL
+ Constraint FK_KhachHang Foreign key (MaKhach) References KhachHang(MaKhach),
+)
+go
+
+-- Add keys for table DonHang
+
+ALTER TABLE [DonHang] ADD CONSTRAINT [PK_DonHang] PRIMARY KEY ([MaDonHang],[MaKhach])
+go
+-------------
+CREATE TABLE Loai
+(
+ MaLoai Int IDENTITY(1,1) NOT FOR REPLICATION NOT NULL primary key,
+ TenLoai Nvarchar(100) NULL
+)
+-----------------
+CREATE TABLE NhaSanXuat
+(
+ MaNhaSX Int IDENTITY(1,1) NOT FOR REPLICATION NOT NULL primary key,
+ TenNhaSX Nvarchar(100) NULL,
+ SoDiaChi Nvarchar(100) NULL,
+ SoDienThoai Varchar(100) NULL,
+ DiaChiEmail Varchar(100) NULL
+)
+
+-- Table SanPham
+
+CREATE TABLE SanPham
+(
+ MaSanPham Int IDENTITY(1,1) NOT FOR REPLICATION NOT NULL primary key,
+ TenSanPham Nvarchar(100) NULL,
+ GiaBan Int NULL,
+ AnhSanPham Varchar(max) NULL,
+ MoTa Nvarchar(max) NULL,
+ SoLuong Tinyint NULL,
+ TinhTrang Nvarchar(100) NULL,
+ MaLoai Int NOT NULL,
+ MaNhaSX Int NOT NULL,
+ Constraint FK_Loai Foreign key (MaLoai) References Loai(MaLoai),
+ Constraint FK_NhaSanXuat Foreign key (MaNhaSX) References NhaSanXuat(MaNhaSX),
+)
+go
+
+-- Add keys for table SanPham
+
+ALTER TABLE [SanPham] ADD CONSTRAINT [PK_SanPham] PRIMARY KEY ([MaSanPham],[MaLoai],[MaNhaSX])
+go
+
+-- Table Loai
+
+
+go
+
+-- Add keys for table Loai
+
+ALTER TABLE [Loai] ADD CONSTRAINT [PK_Loai] PRIMARY KEY ([MaLoai])
+go
+
+-- Table NhaSanXuat
+
+
+go
+
+-- Add keys for table NhaSanXuat
+
+ALTER TABLE [NhaSanXuat] ADD CONSTRAINT [PK_NhaSanXuat] PRIMARY KEY ([MaNhaSX])
+go
+
+-- Table ChiTietHoaDon
+
+CREATE TABLE ChiTietHoaDon
+(
+ MaDonHang Int NOT NULL,
+ MaSanPham Int NOT NULL,
+ SoLuongMua Tinyint NULL,
+ Dongia int check(Dongia>=0),
+ NgayMua Date NULL,
+ Ngaygiao Date NULL,
+ Constraint PK_ChiTietHoaDon PRIMARY KEY( MaDonHang,MaSanPham),
+ Constraint FK_MaDonHang Foreign key (MaDonHang) References DonHang(MaDonHang),
+ Constraint FK_SanPham Foreign key (MaSanPham) References SanPham(MaSanPham),
+)
+go
+
+-- Add keys for table ChiTietHoaDon
+
+ALTER TABLE [ChiTietHoaDon] ADD CONSTRAINT [PK_ChiTietHoaDon] PRIMARY KEY ([MaDonHang],[MaSanPham])
+go
+
+-- Create foreign keys (relationships) section ------------------------------------------------- 
+
+
+ALTER TABLE [DonHang] ADD CONSTRAINT [Relationship147] FOREIGN KEY ([MaKhach]) REFERENCES [KhachHang] ([MaKhach]) ON UPDATE NO ACTION ON DELETE NO ACTION
+go
+
+
+
+ALTER TABLE [ChiTietHoaDon] ADD CONSTRAINT [Relationship148] FOREIGN KEY ([MaDonHang], [MaKhach]) REFERENCES [DonHang] ([MaDonHang], [MaKhach]) ON UPDATE NO ACTION ON DELETE NO ACTION
+go
+
+
+
+ALTER TABLE [ChiTietHoaDon] ADD CONSTRAINT [Relationship149] FOREIGN KEY ([MaSanPham], [MaLoai], [MaNhaSX]) REFERENCES [SanPham] ([MaSanPham], [MaLoai], [MaNhaSX]) ON UPDATE NO ACTION ON DELETE NO ACTION
+go
+
+
+
+ALTER TABLE [SanPham] ADD CONSTRAINT [Relationship150] FOREIGN KEY ([MaLoai]) REFERENCES [Loai] ([MaLoai]) ON UPDATE NO ACTION ON DELETE NO ACTION
+go
+
+
+
+ALTER TABLE [SanPham] ADD CONSTRAINT [Relationship151] FOREIGN KEY ([MaNhaSX]) REFERENCES [NhaSanXuat] ([MaNhaSX]) ON UPDATE NO ACTION ON DELETE NO ACTION
+go
+
+
+
+
+insert into NhaSanXuat values(N'SamSung',N'USA','098995865','Samsung123@gmail.com')
+insert into NhaSanXuat values(N'Alppe',N'USA','098945696','Alppe123@gmail.com')
+
+insert into Loai values(N'Điện Thoại Động')
+insert into Loai values(N'Máy Tính Bản')
+insert into Loai values(N'Laptop')
+insert into Loai values(N'Ipop')
+
+insert into SanPham values(N'SamSung Galaxy A12',4500000,'SamA12.jpn',N'Màu đẹp',50,N'Mới',1,1)
+insert into SanPham values(N'SamSung Galaxy A51',2500000,'SamA51.jpn',N'Màu đẹp',50,N'Mới',1,1)
+insert into SanPham values(N'SamSung Galaxy Z Fold2',5000000,'SamAZF.jpn',N'Màu đẹp',50,N'Mới',1,1)
+insert into SanPham values(N'SamSung Galaxy S21',23900000,'SamS21.jpn',N'Màu đẹp',50,N'Mới',1,1)
+insert into SanPham values(N'SamSung Galaxy S21 Ultra',12000000,'SamS21U.jpn',N'Màu đẹp',50,N'Mới',1,1)
+insert into SanPham values(N'SamSung Galaxy A52',4500000,'SamA52.jpn',N'Màu đẹp',50,N'Mới',1,1)
+
+insert into SanPham values(N'Iphone 12 Pro',22000000,'I12.jpn',N'Màu đẹp',50,N'Mới',1,2)
+insert into SanPham values(N'Iphone 11',20000000,'I11.jpn',N'Màu đẹp',50,N'Mới',1,2)
---
+++ MvcBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,71 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        LinhKienDienTuDataContext data = new LinhKienDienTuDataContext();
+        // GET: BookStore
+
+        private List<SanPham> Laysachmoi(int count)
+        {
+            return data.SanPhams.OrderByDescending(a => a.TinhTrang).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var sachmoi = Laysachmoi(5);
+            return View(sachmoi);
+        }
+
+        public ActionResult ChuDe()
+        {
+            var chude = from cd in data.Loais select cd;
+            return PartialView(chude);
+        }
+
+        public ActionResult Nhaxuatban()
+        {
+            var nhaXuatBan = from cd in data.NhaSanXuats select cd;
+            return PartialView(nhaXuatBan);
+        }
+
+        //=============
+        private string getTenLoai(int? id)
+        {
+            var tenloai = from s in data.Loais where s.MaLoai == id select s.TenLoai;
+            return tenloai.FirstOrDefault();
+        }
+
+        public ActionResult SPTheoLoai(int id)
+        {
+            var sp = from s in data.SanPhams where s.MaLoai == id select s;
+            ViewBag.tenloai = getTenLoai(id);
+            return View(sp);
+        }
+        //===================
+        private string getTenNSX(int? id)
+        {
+            var tenNSX = from s in data.NhaSanXuats where s.MaNhaSX == id select s.TenNhaSX;
+            return tenNSX.FirstOrDefault();
+        }
+
+        public ActionResult SPTheoNSX(int id)
+        {
+            var sp = from s in data.SanPhams where s.MaNhaSX == id select s;
+            ViewBag.tenNSX = getTenNSX(id);
+            return View(sp);
+        }
+        //=================
+        public ActionResult Details(int? id)
+        {
+            if (id == null) return HttpNotFound("Sản phẩm không tồn tại");
+            var sp = from s in data.SanPhams where s.MaSanPham == id select s;
+            return View(sp.Single());
+        }
+    }
+}
